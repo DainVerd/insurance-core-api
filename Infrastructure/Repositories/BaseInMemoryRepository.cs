57eab2c2d11b1ebdd6bccd67df63fc5565a190df
DainVerd <zobs10@inbox.lv>
2026-07-08 10:38:31 +0300

    feat: generic repository for future domain repositories

---
+++ Infrastructure/Repositories/BaseInMemoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces;
+using Domain.Interfaces;
+using System.Collections.Concurrent;
+
+namespace Infrastructure.Repositories;
+
+public abstract class BaseInMemoryRepository<T> : IRepository<T> where T : class, IEntity
+{
+    protected readonly ConcurrentDictionary<Guid, T> Storage = new();
+
+    public T? GetById(Guid id)
+    {
+        Storage.TryGetValue(id, out var entity);
+
+        return entity;
+    }
+
+    public void Add(T entity)
+    {
+        if (!Storage.TryAdd(entity.Id, entity))
+            throw new InvalidOperationException($"Entity {typeof(T).Name} with Id {entity.Id} already exists.");
+    }
+
+    public void Update(T entity)
+    {
+        Storage[entity.Id] = entity;
+    }
+
+    public void Delete(Guid id)
+    {
+        Storage.TryRemove(id, out _);
+    }
+}
