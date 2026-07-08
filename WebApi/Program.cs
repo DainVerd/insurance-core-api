e62fc5a7b5579e53d38328ce37aaa84167a8e48b
DainVerd <zobs10@inbox.lv>
2026-07-08 09:42:13 +0300

    added clean architecture project structure

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,54 +1,68 @@
 using Asp.Versioning;
 using Asp.Versioning.ApiExplorer;
+using Infrastructure;
+using Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-// api versioning
-builder.Services.AddApiVersioning(options =>
+try
 {
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.ReportApiVersions = true;
+    // project dependencies in Clean Architecture pattern
+    builder.Services
+    .AddApplication()
+    .AddInfrastructure();
 
-    options.ApiVersionReader = ApiVersionReader.Combine(
-        new UrlSegmentApiVersionReader(),
-        new HeaderApiVersionReader("x-api-version"),
-        new MediaTypeApiVersionReader("x-api-version")
-    );
-})
-.AddApiExplorer(options =>
-{
-    options.GroupNameFormat = "'v'VVV";
-    options.SubstituteApiVersionInUrl = true;
-});
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
 
-
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
+    // api versioning
+    builder.Services.AddApiVersioning(options =>
     {
-        var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ReportApiVersions = true;
 
-        foreach (var description in provider.ApiVersionDescriptions)
-        {
-            options.SwaggerEndpoint(
-                $"/swagger/{description.GroupName}/swagger.json",
-                description.GroupName.ToUpperInvariant()
-            );
-        }
-
-        options.RoutePrefix = string.Empty;
+        options.ApiVersionReader = ApiVersionReader.Combine(
+            new UrlSegmentApiVersionReader(),
+            new HeaderApiVersionReader("x-api-version"),
+            new MediaTypeApiVersionReader("x-api-version")
+        );
+    })
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
     });
-}
 
-app.MapControllers();
-app.Run();
+
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerEndpoint(
+                    $"/swagger/{description.GroupName}/swagger.json",
+                    description.GroupName.ToUpperInvariant()
+                );
+            }
+
+            options.RoutePrefix = string.Empty;
+        });
+    }
+
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
