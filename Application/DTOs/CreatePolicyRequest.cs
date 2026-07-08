67931bc81dee7d144a57dac22e655e11491361cc
DainVerd <zobs10@inbox.lv>
2026-07-08 12:35:22 +0300

    feat: endpoint to create Policies

---
+++ Application/DTOs/CreatePolicyRequest.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Constants;
+
+namespace Application.DTOs;
+
+public class CreatePolicyRequest
+{
+    public Guid CustomerId { get; set; }
+    public PolicyProductType ProductType { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public decimal Premium { get; set; }
+}
