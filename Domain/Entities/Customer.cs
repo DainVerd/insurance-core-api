da1800d96670632b7494d72d879170129b6fb0ab
DainVerd <zobs10@inbox.lv>
2026-07-08 10:18:53 +0300

    feat: domain entities and enums

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Interfaces;
+
+namespace Domain.Entities;
+
+public class Customer : IEntity
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+}
