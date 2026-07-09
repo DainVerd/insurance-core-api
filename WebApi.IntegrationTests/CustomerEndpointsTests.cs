a3e86cc3f4b96a17c05e696ecf300b672fafefb0
DainVerd <zobs10@inbox.lv>
2026-07-09 14:58:24 +0300

    added integration tests

---
+++ WebApi.IntegrationTests/CustomerEndpointsTests.cs
@@ -0,0 +1,86 @@
+﻿using Application.DTOs;
+using AwesomeAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace WebApi.IntegrationTests;
+
+public class CustomerEndpointsTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public CustomerEndpointsTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task CreateCustomer_Returns201_WhenValidRequest()
+    {
+        // Arrange
+        var request = new { FullName = "Test Customer" };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/v1.0/customers", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    public async Task CreateCustomer_Returns400_WhenInvalidRequest(string? fullName)
+    {
+        // Arrange
+        var request = new { FullName = fullName };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/v1.0/customers", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task GetCustomer_Returns200_WhenFoundCustomer()
+    {
+        // Arrange
+        var request = new { FullName = "Test Customer" };
+
+        // Act
+        var customersCreateResponse = await _client.PostAsJsonAsync("/api/v1.0/customers", request);
+        var customerId = await customersCreateResponse.Content.ReadFromJsonAsync<Guid>();
+
+        var response = await _client.GetAsync($"/api/v1.0/customers/{customerId}");
+        var customerDto = await response.Content.ReadFromJsonAsync<CustomerDto>();
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        customerDto?.Id.Should().Be(customerId);
+    }
+
+    [Fact]
+    public async Task GetCustomer_Returns404_WhenNotFoundCustomer()
+    {
+
+        // Arrange & Act
+        var response = await _client.GetAsync($"/api/v1.0/customers/{Guid.NewGuid()}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetCustomer_Returns400_WhenInvalidCustomerIdProvided()
+    {
+
+        // Arrange & Act
+        var response = await _client.GetAsync($"/api/v1.0/customers/{Guid.Empty}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+}
--- WebApi.IntegrationTests/PolicyEndpointsTests.cs
+++ WebApi.IntegrationTests/PolicyEndpointsTests.cs
@@ -1,4 +1,5 @@
-﻿using AwesomeAssertions;
+﻿using Application.DTOs;
+using AwesomeAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using System.Net.Http.Json;
@@ -15,15 +16,68 @@ public class PolicyEndpointsTests : IClassFixture<WebApplicationFactory<Program>
     }
 
     [Fact]
-    public async Task CreateCustomer_ValidRequest_Returns201()
+    public async Task ActivatePolicy_Returns409_WhenAlreadyActivePolicy()
     {
-        // Arrange
-        var request = new { FullName = "Test Customer" };
+        // Arrange 
+        // creating customer
+        var customerResponse = await _client.PostAsJsonAsync("/api/v1.0/customers", new { FullName = "John Doe" });
+        var customerId = await customerResponse.Content.ReadFromJsonAsync<Guid>();
 
-        // Act
-        var response = await _client.PostAsJsonAsync("/api/v1.0/customers", request);
+        var policyRequest = new CreatePolicyRequest
+        {
+            CustomerId = customerId,
+            ProductType = Domain.Constants.PolicyProductType.Auto,
+            StartDate = new DateOnly(2026,01,01),
+            EndDate = new DateOnly(2026,01,31),
+            Premium = 500
+        };
+        var policyResponse = await _client.PostAsJsonAsync("/api/v1.0/policies", policyRequest);
+        var policyId = await policyResponse.Content.ReadFromJsonAsync<Guid>();
+
+        // Act 
+        var firstActivate = await _client.PostAsync($"/api/v1.0/policies/{policyId}/activate", null);
+
+        var secondActivate = await _client.PostAsync($"/api/v1.0/policies/{policyId}/activate", null);
 
         // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        firstActivate.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        secondActivate.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
+    [Fact]
+    public async Task ActivatePolicy_OverlappingActivePolicySameCustomerAndProductType_Returns409()
+    {
+        // Arrange — customer
+        var customerResponse = await _client.PostAsJsonAsync("/api/v1.0/customers", new { FullName = "John Doe" });
+        var customerId = await customerResponse.Content.ReadFromJsonAsync<Guid>();
+
+        var firstPolicyRequest = new CreatePolicyRequest
+        {
+            CustomerId = customerId,
+            ProductType = Domain.Constants.PolicyProductType.Auto,
+            StartDate = new DateOnly(2026, 01, 01),
+            EndDate = new DateOnly(2026, 01, 31),
+            Premium = 500
+        };
+        var firstPolicyResponse = await _client.PostAsJsonAsync("/api/v1.0/policies", firstPolicyRequest);
+        var firstPolicyId = await firstPolicyResponse.Content.ReadFromJsonAsync<Guid>();
+        await _client.PostAsync($"/api/v1.0/policies/{firstPolicyId}/activate", null);
+
+        // second policy with overlaping date
+        var secondPolicyRequest = new
+        {
+            CustomerId = customerId,
+            ProductType = Domain.Constants.PolicyProductType.Auto,
+            StartDate = new DateOnly(2026, 01, 15),
+            EndDate = new DateOnly(2026, 04, 27),
+            Premium = 300
+        };
+        var secondPolicyResponse = await _client.PostAsJsonAsync("/api/v1.0/policies", secondPolicyRequest);
+        var secondPolicyId = await secondPolicyResponse.Content.ReadFromJsonAsync<Guid>();
+
+        // Act
+        var activateSecond = await _client.PostAsync($"/api/v1.0/policies/{secondPolicyId}/activate", null);
+
+        // Assert
+        activateSecond.StatusCode.Should().Be(HttpStatusCode.Conflict);
     }
 }
