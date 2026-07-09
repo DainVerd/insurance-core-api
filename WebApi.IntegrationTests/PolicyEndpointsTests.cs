b52b654732a406c7630ac168cc21d0a9e412d8f8
DainVerd <zobs10@inbox.lv>
2026-07-09 14:38:01 +0300

    added integration tests project

---
+++ WebApi.IntegrationTests/PolicyEndpointsTests.cs
@@ -0,0 +1,29 @@
+﻿using AwesomeAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace WebApi.IntegrationTests;
+
+public class PolicyEndpointsTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public PolicyEndpointsTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task CreateCustomer_ValidRequest_Returns201()
+    {
+        // Arrange
+        var request = new { FullName = "Test Customer" };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/v1.0/customers", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+}
